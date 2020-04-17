506c736e013746e3658469328cfd1bb52ea69bfc
Marcelobbl <produtormarcelo@hotmail.com>
2020-04-17 10:47:21 -0300

    Interfaces Dao

---
+++ src/model/dao/VendedorDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Vendedor;
+
+public interface VendedorDao {
+
+	void insert(Vendedor obj);
+	void update(Vendedor obj);
+	void delete(Integer id);
+	Vendedor consultaId(Integer id);
+	List<Vendedor> consultaTudo();
+
+}
